e4a9015e983ae1f091716429219a03d322c268fd
Arpita <arpitagolu71@gmail.com>
2026-04-07 16:51:30 +0530

    finding the prime number

---
+++ prime.js
@@ -0,0 +1,9 @@
+function isPrime(num){
+    if(num<2) return false;
+    for (let i =2 ;i<num;i++){
+        if( num%i==0)
+            return false;
+    }
+    return true;
+} 
+console.log(isPrime("3","4","43","55","85","56","89"))
