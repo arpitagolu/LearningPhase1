475406ec6637b3da4a44a9d4e3bb3eac0d20010a
Arpita <arpitagolu71@gmail.com>
2026-04-07 16:42:18 +0530

    finding maxium number

---
+++ number.js
@@ -0,0 +1,10 @@
+function findMax(...arr){
+    let max= arr[0];
+    for(let num of arr){
+        if( num >max )
+            max= num ;
+    }
+    return max;
+}
+console.log(findMax("1","2","3","63","64","87","92"))
+
